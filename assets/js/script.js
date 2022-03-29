d55ed41f0e28c8ec1e225f33ea7075727c3dbf01
bennettem <emily.pamelabennett@gmail.com>
2022-03-28 19:58:13 -0500

    questions for quiz

---
+++ assets/js/script.js
@@ -0,0 +1,58 @@
+// question arrays
+var questions = [
+  {
+    question: "What does CSS stand for?",
+    choices: [
+      "a. Computer Styling Sheets",
+      "b. Creative Sheet Styling",
+      "c. Cascading Style Sheets",
+      "d. Cascading Sheet Stylings",
+    ],
+    answer: 2,
+  },
+  {
+    question: "Arrays in JavaScript can be used to store what?",
+    choices: [
+      "a. Numbers in strings",
+      "b. Booleans",
+      "c. Other Arrays",
+      "d. All of the above",
+    ],
+    answer: 3,
+  },
+  {
+    question: "What does HTML stand for?",
+    choices: [
+      "a. Hyper Text Markup Language",
+      "b. Home Tool Markup Language",
+      "c. HyperLink Markup Language",
+      "d. Hocus Magic Language",
+    ],
+    answer: 0,
+  },
+  {
+    question: "How do you create a function in Javascript?",
+    choices: [
+      "a. function = myfunction()",
+      "b. function myfunction()",
+      "c. myFunction = function()",
+      "d. function myfunction{}",
+    ],
+    answer: 1,
+  },
+  {
+    question: "How would you set a link to open to a new browser window?",
+    choices: [
+      "a. <a href'url' target='new'>",
+      "b. <a href='url' target='_blank'>",
+      "c. <a href='url' new>",
+      "d. <a href='url' target='new.window'>",
+    ],
+    answer: 1,
+  },
+  {
+    question: "What are string values enclosed with?",
+    choices: ["a. Commas", "b. Curly brackets", "c. Quotes", "d. Parentheses"],
+    answer: 2,
+  },
+];
