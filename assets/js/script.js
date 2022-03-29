e2ae1a7b2163e6e97241172d0daba36a999d2e4f
bennettem <emily.pamelabennett@gmail.com>
2022-03-28 20:50:13 -0500

    quiz start/end functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// timer and score vars
+var timer = 5;
+var score = 0;
+
 // question arrays
 var questions = [
   {
@@ -56,3 +60,76 @@ var questions = [
     answer: 2,
   },
 ];
+
+var currentQuestionIndex = 0;
+var startBtn = document.querySelector("#start-btn");
+var startPEl = document.getElementById("start-p");
+var countDownEl = document.querySelector("#timer");
+var quizEl = document.getElementById("quiz");
+var quizQuestionEl = document.getElementById("quiz-question");
+var quizAnswersEl = document.getElementById("quiz-answers");
+var initialsEl = document.getElementById("initals");
+var submitBtn = document.getElementById("submit-btn");
+var quizEndEl = document.getElementById("quiz-end");
+
+// function to start timer/quiz
+
+function countDown() {
+  var countDownTimer = setInterval(function () {
+    countDownEl.textContent = timer;
+    timer--;
+    if (timer === 0) {
+      clearInterval(countDownTimer);
+    }
+  }, 1000);
+  startPEl.classList.add("hidden");
+}
+
+function quizContent() {
+  var quizQuestions = questions[currentQuestionIndex].question;
+  quizQuestionEl.textContent = quizQuestions;
+  var quizChoices = questions[currentQuestionIndex].choices;
+  for (var i = 0; i < quizChoices.length; i++) {
+    var quizChoice = quizChoices[i];
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "list-choices";
+    var selectButton = document.createElement("button");
+    selectButton.className = "button-choice";
+    selectButton.textContent = quizChoice;
+    selectButton.setAttribute("selectedIndex", i);
+    selectButton.addEventListener("click", choiceClicked);
+    listItemEl.appendChild(selectButton);
+    quizAnswersEl.appendChild(listItemEl);
+  }
+}
+
+function choiceClicked() {
+  var buttonEl = event.target;
+  if (buttonEl) {
+    var buttonChoice = parseInt(buttonEl.getAttribute("selectedIndex"));
+    var answerChoice = questions[currentQuestionIndex].answer;
+    if (buttonChosen === answerChoice) {
+      score++;
+      ++currentQuestionIndex;
+      clearAnswers();
+      quizContent();
+    } else {
+    }
+  }
+}
+
+function clearAnswers() {
+  var count = quizAnswersEl.childElementCount;
+  for (var i = 0; i < count; i++) {
+    quizAnswersEl.removeChild(quizAnswersEl.childNodes[0]);
+  }
+}
+
+function endQuiz() {
+  quizQuestionEl.classList.add("hidden");
+  quizEndEl.classList.remove("hidden");
+  var finalScoreEl = document.getElementById("final-score");
+  finalScoreEl.textContent = score;
+}
+
+startBtn.addEventListener("click", countDown);
